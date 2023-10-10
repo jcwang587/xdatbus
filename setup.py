101c2220450b1bb96a337a2503c979902312a6c6
Jiacheng Wang <63186896+jcwang587@users.noreply.github.com>
2023-10-10 00:00:31 -0400

    upload all

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-with open('requirements.txt') as f:
+with open(os.path.join(here, 'requirements.txt')) as f:
     required = f.read().splitlines()
 
 VERSION = '0.0.46'

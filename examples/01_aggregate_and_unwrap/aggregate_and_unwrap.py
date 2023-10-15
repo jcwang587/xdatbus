0cf418156db916c28aa3735a694839eebac3b1d7
Jiacheng Wang <63186896+jcwang587@users.noreply.github.com>
2023-10-14 20:31:31 -0400

    upload all

--- examples/01_aggregate_and_unwrap/aggregate_and_unwrap.py
+++
@@ -1,6 +0,0 @@
-import xdatbus as xdb
-
-aimd_path = '../metad'
-
-xdb.f01_aggregate(aimd_path, load_last_xdatcar=True)
-xdb.f02_unwrap('XDATBUS')
--- examples/02_load_report/load_report.py
+++
@@ -1,6 +0,0 @@
-import xdatbus as xdb
-
-aimd_path = '../metad'
-
-cv = xdb.fm03_report_loader(aimd_path, load_last_report=True)
-

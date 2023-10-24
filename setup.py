8415b031ead8bff90c5ee2f9e2d6c51608d92dd7
Jiacheng Wang <63186896+jcwang587@users.noreply.github.com>
2023-10-24 02:40:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with codecs.open(readme_path, encoding="utf-8") as fh:
 with open(requirements_path) as f:
     required = f.read().splitlines()
 
-VERSION = '0.0.88'
+VERSION = '0.0.89'
 DESCRIPTION = 'A python package to analyze XDATCAR files generated from VASP'
 LONG_DESCRIPTION = 'A python package to analyze XDATCAR files generated from VASP'
 

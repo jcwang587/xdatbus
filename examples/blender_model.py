172d1a35e8f1eadff224c64e5e9ca21e80a53c09
Jiacheng Wang <63186896+jcwang587@users.noreply.github.com>
2023-11-03 03:54:21 -0400

    upload all

--- examples/blender_model.py
+++ examples/blender_model.py
@@ -1,61 +1,7 @@
 import molecularnodes as mn
 import bpy
 import os
-from xdatbus import pos2bpdb, realize_instances
-
-
-def clear_scene():
-    # Clear mesh objects
-    bpy.ops.object.select_all(action="DESELECT")
-    bpy.ops.object.select_by_type(type="MESH")
-    bpy.ops.object.delete()
-
-    # Clear light objects
-    bpy.ops.object.select_all(action="DESELECT")
-    bpy.ops.object.select_by_type(type="LIGHT")
-    bpy.ops.object.delete()
-
-    # Clear geometry node groups
-    for node in bpy.data.node_groups:
-        if node.type == "GEOMETRY":
-            bpy.data.node_groups.remove(node)
-
-
-def render_image(engine='eevee', x=1000, y=500, output_path='render.png'):  # Added output_path parameter
-    # setup render engine
-    if engine == "eevee":
-        bpy.context.scene.render.engine = "BLENDER_EEVEE"
-    elif engine == "cycles":
-        bpy.context.scene.render.engine = "CYCLES"
-        try:
-            bpy.context.scene.cycles.device = "GPU"
-        except:
-            print("GPU Rendering not available")
-
-    # Render
-    bpy.context.scene.render.resolution_x = x
-    bpy.context.scene.render.resolution_y = y
-    bpy.context.scene.render.image_settings.file_format = "PNG"
-    bpy.context.scene.render.filepath = output_path  # Use the output_path
-    bpy.ops.render.render(write_still=True)
-
-
-def apply_modifiers_to_mesh(obj):
-    # Ensure we are operating on the correct object
-    bpy.context.view_layer.objects.active = obj
-    obj.select_set(True)
-
-    # Check if the object is a mesh and apply modifiers accordingly
-    if obj.type == 'MESH':
-        for modifier in obj.modifiers:
-            print(f"Applying modifier: {modifier.name}")
-            try:
-                bpy.ops.object.modifier_apply(modifier=modifier.name)
-            except Exception as e:
-                print(f"Failed to apply modifier {modifier.name}: {e}")
-    else:
-        print(f"Cannot apply modifiers to non-mesh object: {obj.name}")
-
+from xdatbus import pos2bpdb, realize_instances, clear_scene, apply_modifiers_to_mesh, render_image
 
 current_dir = os.getcwd()
 poscar_path = os.path.join(current_dir, '../tests/data/poscar/llto.poscar')
--- xdatbus/utils_bpy.py
+++ xdatbus/utils_bpy.py
@@ -1,5 +1,6 @@
 try:
     import bpy
+
     BPY_AVAILABLE = True
 except ImportError:
     bpy = None
@@ -62,3 +63,101 @@ def realize_instances(obj):
 
             # Update scene
             bpy.context.view_layer.update()
+
+
+def clear_scene(mesh=True, lights=True, geometry_nodes=True):
+    """
+    Clear the scene of all objects. This function requires bpy.
+
+        Parameters
+        ----------
+        mesh : bool
+            Clear mesh objects
+        lights : bool
+            Clear light objects
+        geometry_nodes : bool
+            Clear geometry node groups
+    """
+    if not BPY_AVAILABLE:
+        raise ImportError("The function `clear_scene` requires bpy. Please install bpy to use this function.")
+
+    if mesh:
+        bpy.ops.object.select_all(action="DESELECT")
+        bpy.ops.object.select_by_type(type="MESH")
+        bpy.ops.object.delete()
+
+    if lights:
+        bpy.ops.object.select_all(action="DESELECT")
+        bpy.ops.object.select_by_type(type="LIGHT")
+        bpy.ops.object.delete()
+
+    if geometry_nodes:
+        for node in bpy.data.node_groups:
+            if node.type == "GEOMETRY":
+                bpy.data.node_groups.remove(node)
+
+
+def render_image(engine='eevee', x=1000, y=500, output_path='render.png'):
+    """
+    Render an image. This function requires bpy.
+
+        Parameters
+        ----------
+        engine : str
+            The render engine to use. Either 'eevee' or 'cycles'
+        x : int
+            The x resolution of the image
+        y : int
+            The y resolution of the image
+        output_path : str
+            The path to save the image to
+    """
+    if not BPY_AVAILABLE:
+        raise ImportError("The function `render_image` requires bpy. Please install bpy to use this function.")
+
+    # setup render engine
+    if engine == "eevee":
+        bpy.context.scene.render.engine = "BLENDER_EEVEE"
+    elif engine == "cycles":
+        bpy.context.scene.render.engine = "CYCLES"
+        try:
+            bpy.context.scene.cycles.device = "GPU"
+        except:
+            bpy.context.scene.cycles.device = "CPU"
+
+    # Render
+    bpy.context.scene.render.resolution_x = x
+    bpy.context.scene.render.resolution_y = y
+    bpy.context.scene.render.image_settings.file_format = "PNG"
+    bpy.context.scene.render.filepath = output_path
+
+    bpy.ops.render.render(write_still=True)
+
+
+def apply_modifiers_to_mesh(obj):
+    """
+    Apply modifiers to the given object. This function requires bpy.
+
+        Parameters
+        ----------
+        obj : bpy.types.Object
+            The object to apply modifiers to
+    """
+    if not BPY_AVAILABLE:
+        raise ImportError("The function `apply_modifiers_to_mesh` requires bpy. Please install bpy to use this "
+                          "function.")
+
+    # Ensure we are operating on the correct object
+    bpy.context.view_layer.objects.active = obj
+    obj.select_set(True)
+
+    # Check if the object is a mesh and apply modifiers accordingly
+    if obj.type == 'MESH':
+        for modifier in obj.modifiers:
+            print(f"Applying modifier: {modifier.name}")
+            try:
+                bpy.ops.object.modifier_apply(modifier=modifier.name)
+            except Exception as e:
+                print(f"Failed to apply modifier {modifier.name}: {e}")
+    else:
+        print(f"Cannot apply modifiers to non-mesh object: {obj.name}")

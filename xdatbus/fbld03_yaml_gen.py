7971fb85bb11b20ab29d8d7ca467038379569452
Jiacheng Wang <63186896+jcwang587@users.noreply.github.com>
2023-11-04 03:36:56 -0400

    upload all

---
+++ xdatbus/fbld03_yaml_gen.py
@@ -0,0 +1,44 @@
+import yaml
+
+# Define your configuration template with comments
+config_template = {
+    '# service_host': 'The host of the service in the format host:port',
+    'service_host': 'localhost:8080',
+
+    '# logging_level': 'Logging level. Options: DEBUG, INFO, WARNING, ERROR, CRITICAL',
+    'logging_level': 'INFO',
+
+    '# database': 'Database configuration section',
+    'database': {
+        '# host': 'Database host address',
+        'host': 'localhost',
+
+        '# port': 'Database port number',
+        'port': 3306,
+
+        '# username': 'Username for the database',
+        'username': 'user',
+
+        '# password': 'Password for the database',
+        'password': 'pass',
+    },
+
+    '# server': 'Server configuration section',
+    'server': {
+        '# host': 'Server host address',
+        'host': '0.0.0.0',
+
+        '# port': 'Server port number',
+        'port': 8080,
+    }
+}
+
+
+# Function to generate the YAML template file
+def yaml_gen(filepath, template):
+    with open(filepath, 'w') as file:
+        yaml.dump(template, file, default_flow_style=False, sort_keys=False)
+
+
+# Generate YAML file
+yaml_gen('config_template.yaml', config_template)

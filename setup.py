6ccde7112122ff28ef3cd26e2e60b4baeef83ac9
Jiacheng Wang <kashkingwong@gmail.com>
2023-11-07 13:30:13 -0500

    add test

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ LONG_DESCRIPTION = 'A python package to analyze XDATCAR files generated from VAS
 
 setup(
     name="xdatbus",
-    version="0.0.111",
+    version="0.0.112",
     author="Jiacheng Wang",
     author_email="jiachengwang@umass.edu",
     maintainer="Jiacheng Wang",

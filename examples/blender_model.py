df7a5127bbc80e06158205b307a3886825e9f5e3
Jiacheng Wang <63186896+jcwang587@users.noreply.github.com>
2023-11-08 03:34:32 -0500

    upload all

--- examples/blender_model.py
+++ examples/blender_model.py
@@ -21,7 +21,7 @@ yaml_gen('llto_rm_bond.pdb')
 
 # Load the molecule and apply the style
 clear_scene()
-mol = mn.load.molecule_local("llto_rm_bond.pdb", default_style='stick')
+mol = mn.load.molecule_local("llto_rm_bond.pdb", default_style='ball_and_stick')
 apply_yaml(mol, 'llto_rm_bond_style.yaml')
 
 # Export the scene to a blender file

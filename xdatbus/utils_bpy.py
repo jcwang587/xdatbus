00ee0374338058ffc011c4f42f947bdcd83c4894
Jiacheng Wang <63186896+jcwang587@users.noreply.github.com>
2023-11-08 02:17:20 -0500

    upload all

--- xdatbus/utils_bpy.py
+++ xdatbus/utils_bpy.py
@@ -8,11 +8,25 @@ except ImportError:
     BPY_AVAILABLE = False
 
 try:
+    import biotite.structure.io.pdb as pdb
+    PDB_AVAILABLE = True
+except ImportError:
+    pdb = None
+    PDB_AVAILABLE = False
+
+try:
     import molecularnodes as mn
     MN_AVAILABLE = True
 except ImportError:
     mn = None
     MN_AVAILABLE = False
+
+try:
+    import yaml
+    YAML_AVAILABLE = True
+except ImportError:
+    yaml = None
+    YAML_AVAILABLE = False
 
 
 def realize_instances(obj):
@@ -231,8 +245,6 @@ def apply_yaml(obj, yaml_path):
     set_color4element(obj, 8, (0.8, 0.0, 0.0, 1), 0.8)
 
 
-
-
 def clear_scene(mesh=True, lights=True, geometry_nodes=True):
     """
     Clear the scene of all objects. This function requires bpy.
@@ -329,3 +341,49 @@ def apply_modifiers_to_mesh(obj):
                 print(f"Failed to apply modifier {modifier.name}: {e}")
     else:
         print(f"Cannot apply modifiers to non-mesh object: {obj.name}")
+
+
+def yaml_gen(pdb_file_path):
+    """
+    This function generates a YAML file for the elements in the PDB file.
+
+        Parameters
+        ----------
+        pdb_file_path : str
+            Input path of the PDB file
+    """
+    if not PDB_AVAILABLE:
+        raise ImportError("The function `yaml_gen` requires biotite. Please install biotite to use this function.")
+    if not YAML_AVAILABLE:
+        raise ImportError("The function `yaml_gen` requires yaml. Please install yaml to use this function.")
+
+    # Create a PDBFile object
+    pdb_file = pdb.PDBFile.read(pdb_file_path)
+
+    # Convert PDB file to an AtomArray
+    atom_array = pdb.get_structure(pdb_file)[0]
+
+    # Extract the PDB ID from the file name for naming the YAML file
+    pdb_id = pdb_file_path.split('/')[-1].split('.')[0]
+
+    # Get unique elements from the atom array
+    unique_elements = set(atom_array.element.astype(str))  # Convert elements to Python strings
+
+    # Create a dictionary for elements with default color and size scale
+    elements_dict = {
+        str(element): {  # Ensure that element is a Python string
+            'color': [0, 0, 0, 1],  # RGBA for black with full opacity as a list
+            'size_scale': 0.8
+        }
+        for element in unique_elements
+    }
+
+    # Generate YAML string from dictionary
+    # Use default_flow_style=False to output a traditional YAML format
+    yaml_str = yaml.safe_dump(elements_dict, sort_keys=True, default_flow_style=False)
+
+    # Write YAML string to file
+    with open(f'{pdb_id}_style.yaml', 'w') as file:
+        file.write(yaml_str)
+
+    print(f"YAML file '{pdb_id}_style.yaml' has been created.")

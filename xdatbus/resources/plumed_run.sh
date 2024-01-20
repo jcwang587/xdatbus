ba3b914fc6a91556a1c6978beab6a94447e199b9
Jiacheng Wang <63186896+jcwang587@users.noreply.github.com>
2024-01-20 02:03:22 -0500

    upload all

--- xdatbus/resources/plumed_run.sh
+++ xdatbus/resources/plumed_run.sh
@@ -4,17 +4,12 @@ echo "Process ID : $$"
 # Get the name of the current conda environment and directory
 current_env=$(conda info --envs | grep '*' | awk '{print $1}')
 current_dir=$(pwd)
-in_dir="$current_dir/in"
-res_dir="$current_dir/res"
 
-# Check if the current environment is "my_plumed"
-if [ "$current_env" != "my_plumed" ]; then
-    # Activate "my_plumed" environment
-    eval "$(conda shell.bash hook)"
-    conda activate my_plumed
-    echo "Activated 'my_plumed' environment."
+# Check if the 'plumed' package is installed in the current Conda environment
+if conda list | grep -q "^plumed "; then
+    echo "The 'plumed' package is installed in the current environment."
 else
-    echo "The 'my_plumed' environment is already active."
+    echo "The 'plumed' package is not installed in the current environment."
 fi
 
 echo "Began running plumed!"
@@ -29,8 +24,3 @@ fi
 # Run Plumed
 cd "$in_dir"
 plumed sum_hills --hills HILLS --outfile ../res/fes_bias.dat --mintozero --min 0 --max 11.636 --bin 100
-
-# Post process
-ln -sf ~/softwares/plumed/bin/save_bias_fes.plt "$res_dir"
-cd "$res_dir"
-gnuplot save_bias_fes.plt

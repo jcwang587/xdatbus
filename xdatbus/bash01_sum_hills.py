1472a22137b276c1f40815b6ba53b5cfe904b4e9
Jiacheng Wang <63186896+jcwang587@users.noreply.github.com>
2024-01-20 03:33:16 -0500

    upload all

--- xdatbus/bash01_sum_hills.py
+++ xdatbus/bash01_sum_hills.py
@@ -1,8 +1,27 @@
-import subprocess
 import os
+import subprocess
+import argparse
 
 
 def sum_hill():
     current_dir = os.path.dirname(__file__)
     script_path = os.path.join(current_dir, './resources', 'sum_hills.sh')
     subprocess.run(['bash', script_path])
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Aggregate XDATCAR files from an AIMD simulation.")
+    parser.add_argument("--xdc_dir", type=str,
+                        help="Input path of the AIMD simulation, which contains the XDATCAR files")
+    parser.add_argument("--output_path", type=str, default="./",
+                        help="Output path of the XDATBUS file (default: current directory)")
+    parser.add_argument("--delete_temp_files", action="store_true",
+                        help="If set, the intermediate folders will be deleted (default: False)")
+
+    args = parser.parse_args()
+
+    sum_hill()
+
+
+if __name__ == "__main__":
+    main()

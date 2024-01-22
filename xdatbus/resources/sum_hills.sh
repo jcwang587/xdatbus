8cfc588c514f026c367a62484f4cb31b4f2b07a7
Jiacheng Wang <63186896+jcwang587@users.noreply.github.com>
2024-01-22 00:06:24 -0500

    upload all

--- xdatbus/resources/sum_hills.sh
+++ xdatbus/resources/sum_hills.sh
@@ -44,11 +44,12 @@ done
 
 # Validate HILLS file exists
 if [ ! -f "$HILLS" ]; then
-    # Copy the HILLS file to the results folder
-    cp "$HILLS" "$results_dir"/HILLS
     echo "HILLS file not found: $HILLS"
     exit 1
 fi
+
+# Copy the HILLS file to the results folder
+cp "$HILLS" "$results_dir"/HILLS
 
 # Run Plumed with the given arguments
 plumed sum_hills --hills "$HILLS" --outfile "$results_dir"/"$OUTFILE" --mintozero --min "$MIN" --max "$MAX" --bin "$BIN"

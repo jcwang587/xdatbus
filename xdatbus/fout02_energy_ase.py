0abfbe536b5d26ddc8401a475b5ee1f2141c5c37
Jiacheng Wang <63186896+jcwang587@users.noreply.github.com>
2024-02-06 23:59:01 -0500

    upload all

--- xdatbus/fout02_energy_ase.py
+++ xdatbus/fout02_energy_ase.py
@@ -1,3 +1,13 @@
 from pymatgen.io.vasp import Vasprun
 
 vasprun = Vasprun("../tests/data/vasprun/vasprun.xml")
+
+# get the total energy
+energy = vasprun.final_energy
+print(energy)
+
+# get all energies
+for i in range(len(vasprun.ionic_steps)):
+    print(vasprun.ionic_steps[i]['electronic_steps'][-1]['e_wo_entrp'])
+
+# get all kinetic energies

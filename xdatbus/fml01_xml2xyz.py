583474c1fa3b2e75790daf555318282b3e8c6b8d
Jiacheng Wang <63186896+jcwang587@users.noreply.github.com>
2024-02-17 17:47:53 -0500

    upload all

--- xdatbus/fml01_xml2xyz.py
+++ xdatbus/fml01_xml2xyz.py
@@ -21,8 +21,43 @@ def xml2xyz(xml_dir="./", output_path="./"):
         if len(xml_list) == 0:
             raise ValueError("No vasprun.xml file found in the directory.")
 
+        xml_list_sort = sorted(xml_list, key=lambda x: int(re.findall(r"\d+", x)[0]))
+
+        for xml_file in xml_list_sort:
+            print(f"xdatbus-func | xml2xyz: Processing {xml_file}")
+            xml_path = os.path.join(xml_dir, xml_file)
+            xml_set = read(xml_path, index="::")
+            xyz_path = os.path.join(output_path, f"{xml_file}.xyz")
+            write(xyz_path, xml_set)
+
         print("xdatbus-func | xml2xyz: Done!")
 
     except Exception as e:
         print(e)
         print("xdatbus-func | xml2xyz: Failed!")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="XML to XYZ converter")
+
+    parser.add_argument(
+        "--xml_dir",
+        type=str,
+        default="./",
+        help="The directory containing the vasprun.xml files.",
+    )
+
+    parser.add_argument(
+        "--output_path",
+        type=str,
+        default="./",
+        help="The directory to save the extended xyz files.",
+    )
+
+    args = parser.parse_args()
+
+    xml2xyz(args.xml_dir, args.output_path)
+
+
+if __name__ == "__main__":
+    main()

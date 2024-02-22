2d9be5a35286ecdadf961d06a411c4ca483a73b4
Jiacheng Wang <kashkingwong@gmail.com>
2024-02-22 16:29:30 -0500

    add test

--- xdatbus/fcli.py
+++ xdatbus/fcli.py
@@ -5,16 +5,14 @@ from rich.table import Table
 def main():
     console = Console()
 
-    table = Table(show_header=True, header_style="bold magenta")
+    table = Table(title="XDATBUS HELP MENU", show_header=True, header_style="bold magenta")
     table.add_column("Command", style="dim", width=12)
     table.add_column("Description")
 
     # Add rows for each command
     table.add_row("command1", "Description of command1")
     table.add_row("command2", "Description of command2")
-    # Add as many commands as you have
 
-    console.print("xdatbus Help Menu", style="bold green")
     console.print(table)
 
 

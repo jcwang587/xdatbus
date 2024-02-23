6da85a69df0feeba1e139e48b1253202492e8b06
Jiacheng Wang <kashkingwong@gmail.com>
2024-02-23 12:05:43 -0500

    add test

--- xdatbus/fml01_xml2xyz.py
+++ xdatbus/fml01_xml2xyz.py
@@ -40,7 +40,7 @@ def xml2xyz(xml_dir="./", output_path="./", train_ratio=1.0):
 
         data_set = []
         with Progress(console=console) as progress:
-            task = progress.add_task("[cyan]🚌 xdatbus xml2xyz", total=len(xml_list_sort))
+            task = progress.add_task("🚌 xdatbus xml2xyz", total=len(xml_list_sort))
             for xml_file in xml_list_sort:
                 progress.console.log(f"xml2xyz: processing {xml_file}")
                 xml_path = os.path.join(xml_dir, xml_file)

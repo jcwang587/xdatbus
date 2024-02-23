f2a92278d422249d618170f5b33ce73491b39f78
Jiacheng Wang <63186896+jcwang587@users.noreply.github.com>
2024-02-22 23:11:05 -0500

    update README.md

--- xdatbus/fml01_xml2xyz.py
+++ xdatbus/fml01_xml2xyz.py
@@ -21,7 +21,7 @@ def xml2xyz(xml_dir="./", output_path="./", train_ratio=1.0):
             The ratio of training set
     """
 
-    console = Console(log_path=False)
+    console = Console(log_path=False, force_terminal=True)
 
     try:
         raw_list = os.listdir(xml_dir)

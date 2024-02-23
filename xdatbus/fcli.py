6e76647322304b8a8c696015460f1c9f2e1f7888
Jiacheng Wang <63186896+jcwang587@users.noreply.github.com>
2024-02-22 20:54:18 -0500

    update README.md

--- xdatbus/fcli.py
+++ xdatbus/fcli.py
@@ -16,22 +16,22 @@ def main():
     table.add_row(
         "xdc_aggregate",
         "[--xdc_dir] [--output_dir] [--del_temp]",
-        "Aggregate XDATCAR files from an AIMD simulation.",
+        "Aggregate XDATCAR files from an AIMD simulation",
     )
     table.add_row(
         "xdc_unwrap",
         "[--xdc_path] [--output_path]",
-        "Unwrap the coordinates in the XDATCAR file (to xyz).",
+        "Unwrap the coordinates in the XDATCAR file (to xyz)",
     )
     table.add_row(
         "thermal_report",
         "[--osz_dir] [--output_path]",
-        "Generate a thermal report from the OSZICAR files.",
+        "Generate a thermal report from the OSZICAR files",
     )
     table.add_row(
         "xml2xyz",
         "[--xml_dir] [--output_path] [--train_ratio]",
-        "Convert the vasprun.xml files to extended xyz files.",
+        "Convert the vasprun.xml files to extended xyz files",
     )
 
     console.print(table)

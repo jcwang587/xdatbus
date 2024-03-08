91fb8b709ea744c2134358b98baa9495061d859f
Jiacheng Wang <63186896+jcwang587@users.noreply.github.com>
2024-03-08 01:41:54 -0500

    update README.md

--- xdatbus/fmtd01_fes1d.py
+++ xdatbus/fmtd01_fes1d.py
@@ -44,20 +44,20 @@ def calculate_profile_1d(hillspot_path, hills_count, cv_min=8, cv_max=10, resolu
     f.close()
 
     step = (cv_max - cv_min) / resolution
-    x = cv_min
+    cv = cv_min
 
     # Initialize a list to store dictionaries
     data_list = []
 
     for i in range(1, resolution):
         en = 0.0
-        x = x + step
+        cv = cv + step
         for j in range(len(data)):
-            x0 = data[j][0]
-            en_ = gauss_pot(x, x0, h[j], w[j])
+            cv0 = data[j][0]
+            en_ = gauss_pot(cv, cv0, h[j], w[j])
             en += en_
         # Append data to the list
-        data_list.append({"x": x, "potential_energy": en})
+        data_list.append({"cv": cv, "potential_energy": en})
 
     # Convert the list of dictionaries to a DataFrame
     df = pd.DataFrame(data_list)

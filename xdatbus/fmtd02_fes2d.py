b26c55c10e7d3cb6889e16c8d6531c759c611881
Jiacheng Wang <kashkingwong@gmail.com>
2024-03-13 15:09:50 -0400

    add test

--- xdatbus/fmtd02_fes2d.py
+++ xdatbus/fmtd02_fes2d.py
@@ -2,10 +2,24 @@ import pandas as pd
 from xdatbus.utils import gauss_pot
 
 
-def calculate_profile_1d(hillspot_path, hills_count, cv_min=8, cv_max=10, resolution=1000):
+def calculate_profile_1d(hillspot_path, hills_count, cv_min, cv_max, resolution=1000):
     """
     Calculate the 1D free energy profile from a HILLSPOT file.
+
+        Parameters
+        ----------
+        hillspot_path : str
+            The path of the HILLSPOT file
+        hills_count : int
+            The number of hills to be read
+        cv_min : float
+            The minimum value of the collective variable
+        cv_max : float
+            The maximum value of the collective variable
+        resolution : int (optional)
+            The resolution of the free energy profile
     """
+
     f = open(hillspot_path, "r")
 
     data = []
@@ -29,7 +43,6 @@ def calculate_profile_1d(hillspot_path, hills_count, cv_min=8, cv_max=10, resolu
     step = (cv_max - cv_min) / resolution
     cv = cv_min
 
-    # Initialize a list to store dictionaries
     data_list = []
 
     for i in range(1, resolution):
@@ -39,14 +52,18 @@ def calculate_profile_1d(hillspot_path, hills_count, cv_min=8, cv_max=10, resolu
             cv0 = data[j][0]
             en_ = gauss_pot(cv, cv0, h[j], w[j])
             en += en_
-        # Append data to the list
         data_list.append({"cv": cv, "potential_energy": en})
 
-    # Convert the list of dictionaries to a DataFrame
     df = pd.DataFrame(data_list)
 
     return df
 
 
 input_line_number = 10
-fes = calculate_profile_1d("../tests/data/hillspot/HILLSPOT", input_line_number)
+fes = calculate_profile_1d(
+    "../tests/data/hillspot/HILLSPOT",
+    input_line_number,
+    cv_min=0,
+    cv_max=10,
+    resolution=1000,
+)

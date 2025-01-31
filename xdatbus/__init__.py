270d4c8279433dda9dbf89dfa8ec5cc54d385d4b
jcwang587 <jiachengwang@umass.edu>
2025-01-31 12:29:39 -0500

    rename fun

--- xdatbus/__init__.py
+++ xdatbus/__init__.py
@@ -2,18 +2,15 @@ import warnings
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-from .f01_xdc_aggregate import *
-from .f02_xdc_unwrap import *
-from .f03_xyz_unwarp import *
-from .f04_thermal_report import *
+from .c01_xdc_aggregate import *
+from .c02a_xdc_unwrap import *
+from .c02b_xyz_unwarp import *
+from .c03_thermal_report import *
+from .c04_xml2xyz import *
+from .c05_sum_hills import *
 
 from .fun_com import *
-
-from .fml01_xml2xyz import *
-
 from .fun_mtd import *
-
-from .bash01_bias import *
 
 from .utils import *
 from .utils_bpy import *
